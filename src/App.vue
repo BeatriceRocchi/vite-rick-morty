2f20f7e9891e23353dbb4485576e46cfa5b52749
BeatriceRocchi <135985146+BeatriceRocchi@users.noreply.github.com>
2024-03-25 14:00:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import { store } from "./data/store";
+
+export default {
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <Main />
+</template>
+
+<style lang="scss">
+@use "./assets/scss/main.scss";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<script>
+import { store } from "../data/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <button @click="store.counter++">Incrementa counter</button>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
