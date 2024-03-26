1662ee83e5bf725f91a81e4791505818800ee2a9
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-03-26 15:38:17 +0100

    [add] species search an [add] status search

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,19 +29,37 @@ export default {
   <header class="text-center">
     <h1 class="my-4">- Rick & Morty -</h1>
     <div class="container d-flex justify-content-center px-5 my-4">
+      <!-- Search by name (input) -->
       <input
         type="text"
         class="form-control me-3 w-25"
         id="searchCharacter"
         placeholder="Search Character"
-        v-model="store.queryParams.name"
-        @keyup="startSearch"
+        v-model.trim="store.queryParams.name"
+        @keyup.enter="startSearch"
       />
-      <select class="form-select me-3 w-25" aria-label="Default select example">
-        <option selected>Select Status</option>
-        <option value="1">Alive</option>
-        <option value="2">Death</option>
-        <option value="3">Unknown</option>
+      <!-- Search by status (select) -->
+      <select
+        class="form-select me-3 w-25"
+        aria-label="Default select example"
+        v-model="store.queryParams.status"
+      >
+        <option disabled value="">Select Status</option>
+        <option value="alive">Alive</option>
+        <option value="death">Death</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <!-- Search by species (select) -->
+      <select
+        class="form-select me-3 w-25"
+        aria-label="Default select example"
+        v-model="store.queryParams.species"
+      >
+        <option disabled value="">Select Species</option>
+        <option value="human">Human</option>
+        <option value="alien">Alien</option>
+        <option value="humanoid">Humanoid</option>
+        <option value="unknown">Unknown</option>
       </select>
       <button class="btn btn-info me-3" @click="startSearch">Search</button>
       <button class="btn btn-warning" @click="resetSearch">Reset</button>
